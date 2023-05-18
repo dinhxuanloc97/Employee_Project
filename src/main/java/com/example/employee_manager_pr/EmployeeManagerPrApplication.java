e50e6777078b534caa898d13b8223580b2002f92
dinhxuanloc97 <dinhxuanloc741997@gmail.com>
2023-05-18 16:41:47 +0700

    lan1

---
+++ src/main/java/com/example/employee_manager_pr/EmployeeManagerPrApplication.java
@@ -0,0 +1,13 @@
+package com.example.employee_manager_pr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagerPrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeManagerPrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/employee_manager_pr/EmployeeManagerPrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employee_manager_pr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagerPrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
